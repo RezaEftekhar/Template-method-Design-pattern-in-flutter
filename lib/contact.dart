05e4a5694f574774592020a1f7c36fe025871652
reza <reza.eftekhar80@gmail.com>
2023-10-30 13:49:02 +0330

    adding contact class

---
+++ lib/contact.dart
@@ -0,0 +1,12 @@
+
+class Contact {
+  final String fullName;
+  final String email;
+  final bool favourite;
+
+  const Contact({
+    required this.fullName,
+    required this.email,
+    required this.favourite,
+  });
+}
