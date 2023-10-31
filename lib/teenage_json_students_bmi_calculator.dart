36361768bcac1be4bcf3e978a2002a8d8b68c710
reza <reza.eftekhar80@gmail.com>
2023-10-31 11:00:10 +0330

    adding json information calculator

---
+++ lib/teenage_json_students_bmi_calculator.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:templatemethodexample/jsonApi.dart';
+import 'package:templatemethodexample/students.dart';
+
+class TeenageStudentsJsonBmiCalculator extends StudentsBmiCalculator {
+  const TeenageStudentsJsonBmiCalculator({
+    this.api = const JsonStudentsApi(),
+  });
+
+  final JsonStudentsApi api;
+
+  @override
+  @protected
+  List<Student> getStudentsData() {
+    final studentsJson = api.getStudentsJson();
+    final studentsMap = json.decode(studentsJson) as Map<String, dynamic>;
+    final studentsJsonList = studentsMap['students'] as List;
+    final studentsList = studentsJsonList.map((json) {
+      final studentJson = json as Map<String, dynamic>;
+
+      return Student(
+        fullName: studentJson['fullName'] as String,
+        age: studentJson['age'] as int,
+        height: studentJson['height'] as double,
+        weight: studentJson['weight'] as int,
+      );
+    }).toList();
+
+    return studentsList;
+  }
+
+  @override
+  @protected
+  List<Student> doStudentsFiltering(List<Student> studentList) {
+    return studentList
+        .where((student) => student.age > 12 && student.age < 20)
+        .toList();
+  }
+}
