2ae05ca30db0363b00988ce0bb389a5769747039
reza <reza.eftekhar80@gmail.com>
2023-11-01 22:53:39 +0330

    calculating students BMI based on json information part

---
+++ lib/students_json_bmi_calculator.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:templatemethodexample/jsonApi.dart';
+import 'package:templatemethodexample/students.dart';
+import 'package:templatemethodexample/students_bmi_calculator.dart';
+
+class StudentsJsonBmiCalculator extends StudentsBmiCalculator {
+  const StudentsJsonBmiCalculator({
+    this.api = const JsonStudentsApi(),
+  });
+
+  final JsonStudentsApi api;
+
+  @override
+  @protected
+  List<Student> getStudentsData() {
+    final studentsJson = api.getStudentsJson();
+    final studentsMap = json.decode(studentsJson) as Map<String, dynamic>;
+    final studentsJsonList = studentsMap['students'] as List;
+    final studentsList = studentsJsonList.map((json) {
+      final studentJson = json as Map<String, dynamic>;
+
+      return Student(
+        fullName: studentJson['fullName'] as String,
+        age: studentJson['age'] as int,
+        height: studentJson['height'] as double,
+        weight: studentJson['weight'] as int,
+      );
+    }).toList();
+
+    return studentsList;
+  }
+}
