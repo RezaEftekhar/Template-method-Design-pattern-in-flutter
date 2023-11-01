38a9c06c871a43824757da08fcd1c2bee42936bd
reza <reza.eftekhar80@gmail.com>
2023-11-01 13:09:15 +0330

    adding xml BMI calculator

---
+++ lib/students_xml_bmi_calculator.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:templatemethodexample/students.dart';
+import 'package:templatemethodexample/students_bmi_calculator.dart';
+import 'package:templatemethodexample/xmlApi.dart';
+import 'package:xml/xml.dart';
+
+class StudentsXmlBmiCalculator extends StudentsBmiCalculator {
+  const StudentsXmlBmiCalculator({
+    this.api = const XmlStudentsApi(),
+  });
+
+  final XmlStudentsApi api;
+
+  @override
+  @protected
+  List<Student> getStudentsData() {
+    final studentsXml = api.getStudentsXml();
+    final xmlDocument = XmlDocument.parse(studentsXml);
+    final studentsList = <Student>[];
+
+    for (final xmlElement in xmlDocument.findAllElements('student')) {
+      final fullName = xmlElement.findElements('fullname').single.innerText;
+      final age = int.parse(xmlElement.findElements('age').single.innerText);
+      final height =
+          double.parse(xmlElement.findElements('height').single.innerText);
+      final weight =
+          int.parse(xmlElement.findElements('weight').single.innerText);
+
+      studentsList.add(
+        Student(
+          fullName: fullName,
+          age: age,
+          height: height,
+          weight: weight,
+        ),
+      );
+    }
+
+    return studentsList;
+  }
+}
