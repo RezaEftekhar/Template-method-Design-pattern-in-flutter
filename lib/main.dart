c14af5936c1957b2903b97bdc445ddb56e0082f9
reza <reza.eftekhar80@gmail.com>
2023-11-02 11:48:46 +0330

    completing UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:templatemethodexample/students_json_bmi_calculator.dart';
+import 'package:templatemethodexample/students_section.dart';
+import 'package:templatemethodexample/students_xml_bmi_calculator.dart';
+import 'package:templatemethodexample/teenage_json_students_bmi_calculator.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: TemplateMethodExampleWidget(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TemplateMethodExampleWidget extends StatelessWidget {
+  const TemplateMethodExampleWidget();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Student BMI Calculator'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              CustomSection(
+                bmiCalculator: StudentsXmlBmiCalculator(),
+                headerText: 'XML Data Source',
+              ),
+              SizedBox(height: 20),
+              CustomSection(
+                bmiCalculator: StudentsJsonBmiCalculator(),
+                headerText: 'JSON Data Source',
+              ),
+              SizedBox(height: 20),
+              CustomSection(
+                bmiCalculator: TeenageStudentsJsonBmiCalculator(),
+                headerText: 'Teenage Students (JSON)',
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
